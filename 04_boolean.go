4700214a695286cbcbdd4d2ade5f23c8d7da30b3
Giffari Alfarizy <giffarialfarizy@gmail.com>
2021-12-08 20:33:10 +0700

    ✨Boolean

---
+++ 04_boolean.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+}
+
+// go build 04_boolean.go
+// ./04_boolean
+
+// go run 04_boolean.go
+
+// Boolean
+	// true
+	// false
+
+
