1a19678b70f9a8326ccb7c98ec3936db37d8552c
Giffari Alfarizy <giffarialfarizy@gmail.com>
2021-12-08 20:39:19 +0700

    ✨String

---
+++ 05_string.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Giffari")
+	fmt.Println("Giffari Alfarizy")
+
+	fmt.Println(len("Giffari"))
+	fmt.Println("Giffari Alfarizy"[0])
+	fmt.Println("giffari Alfarizy"[0])
+}
+
+// go build 05_string.go
+// ./05_string
+
+// String
+	// Gabungan beberapa karakter. 1 atau 2 karakter juga boleh
+	// Diawali petik 2 ""
+
+// Function of String
+	// len("string") => Menghitung jumlah karakter di String
+	// "string"[number] => Mengambil karakter pada posisi yang ditentukan
+
+// Huruf G adalah 71
+// Huruf g adalah 103
