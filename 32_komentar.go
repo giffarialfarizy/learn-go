b268c2b3a5c88dfee0ef0da303ee86fe35cf1da0
Giffari Alfarizy <giffarialfarizy@gmail.com>
2021-12-20 05:58:09 +0700

    ✨ Comment

---
+++ 32_komentar.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+This is multi line comment
+You can add a lot of comments here
+Biasanya digunakan sebagai dokumentasi yang menjelaskan
+cara kerja function, ditulis di atas function
+*/
+func main() {
+	// this is single line comment
+	// Single comment biasanya di dalam function
+}
+
+// Komentar terbaik pada kode adalah kode itu sendiri
+
+// Saat membuat kode, kita perlu membuat kode semudah
+// ...mungkin untuk dibaca
+
+// Namun kadang juga kita butuh menambahkan komentar
+// ...di kode kita
